4d4cf396bc191d9171e7647ebe7f81a0dd86fcc2
PabloEstrellaP <pedro.estrella@megamedia.com.mx>
2024-04-22 09:30:37 -0600

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout>
+    <v-app>
+      <transition name="fade">
+          <v-container grid-list-xl>
+            <NuxtPage />
+          </v-container>
+        </transition>
+    </v-app>
+  </NuxtLayout>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-layout row wrap>
+        <v-row>
+            <v-flex v-for="i in [1,2,3,4]" :key="i"   class="pa-5 xs12 md4" >
+                <cCardVue />        
+            </v-flex>
+        </v-row>
+    </v-layout>
+</template>
+
+<script>
+import cCardVue from "~/components/cCard.vue";
+
+export default {
+  components: {
+    cCardVue,
+  },
+};
+</script>
+
+<style>
+</style>
